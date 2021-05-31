cdadb9b0dd33c932462fe387298eceb8378cc56c
DouglasTeles <douglastelesoliveira@gmail.com>
2021-05-31 15:58:56 -0300

    bd

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV == "production") {
-    module.exports = {mongoURI: "mongodb://douglas:admin123@cluster0.ipwbz.mongodb.net/test"}
+    module.exports = {mongoURI: "mongodb+srv://douglas:admin123@cluster0.ipwbz.mongodb.net/blogapp"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
